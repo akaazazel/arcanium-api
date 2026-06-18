15031e0ba2bc8cdfa52618ead018ada975470089
akaazazel <cvakshay764@gmail.com>
2026-06-18 12:55:33 +0530

    test: improved testing note creation service. - added session scoped fixtures for database creation - test valid note creations - test note creation without user

--- tests/test_service_notes.py
+++ tests/test_service_notes.py
@@ -5,41 +5,58 @@ from app.models.models import *
 from app.schemas.schemas import NoteCreate, UserCreate
 from app.services.notes import create_note, decrypt, get_note
 from app.utils.auth import hash_password
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine, AsyncSession
 from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy import select, text
+from sqlalchemy.exc import IntegrityError
 
 postgre_url = "postgresql+psycopg://postgres:unsettled@postgres:5432/arcanium_test_db"
 engine = create_async_engine(postgre_url)
 Session = async_sessionmaker(bind=engine, expire_on_commit=False)
 
 
-@pytest_asyncio.fixture
-async def db():
+# fixtures
+
+
+@pytest_asyncio.fixture(scope="session")
+async def init_db():
     if not database_exists(postgre_url):
         create_database(postgre_url)
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    async with Session() as session:
-        yield session
+    yield
 
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
+    await engine.dispose()
 
-@pytest.fixture
+
+@pytest_asyncio.fixture  # creates tables and yields a db session, lastly drops tables
+async def db(init_db):
+    async with Session() as session:
+        yield session
+
+    await session.execute(
+        text('TRUNCATE TABLE "note", "user" RESTART IDENTITY CASCADE')
+    )
+    await session.commit()
+
+
+@pytest.fixture  # returns user data
 def user_data():
     return UserCreate(name="akshay", email="akshay@gmail.com", password="unsettling")
 
 
-@pytest.fixture
+@pytest.fixture  # returns note data
 def note_data():
     return NoteCreate(title="Note title 01", content="Note content 01")
 
 
-@pytest.mark.asyncio
-async def test_create_note(db, user_data, note_data):
+@pytest_asyncio.fixture  # adds a dummy user to database and returns the db object of user
+async def added_user(user_data, db):
     user = User(
         name=user_data.name,
         email=user_data.email,
@@ -50,10 +67,35 @@ async def test_create_note(db, user_data, note_data):
     await db.commit()
     await db.refresh(user)
 
+    return user
+
+
+# tests
+
+
+@pytest.mark.asyncio  # test valid note
+async def test_create_note(db: AsyncSession, note_data, added_user):
+    user = added_user
+
+    # create a note
     note_id = await create_note(note_data=note_data, user_id=user.id, db=db)
 
-    fetched_note = await get_note(note_id=note_id, user_id=user.id, db=db)
+    # fetch the note
+    result = await db.execute(select(Note).where(Note.id == note_id))
+    fetched_note = result.scalars().first()
 
+    assert fetched_note is not None
     assert fetched_note.id == note_id
     assert decrypt(fetched_note.title) == note_data.title
     assert decrypt(fetched_note.content) == note_data.content
+    assert fetched_note.owner == user.id
+    assert fetched_note.created_at is not None
+    assert fetched_note.updated_at is not None
+    assert fetched_note.updated_at == fetched_note.created_at
+
+
+@pytest.mark.asyncio  # create note without a user
+async def test_create_note_no_user(db: AsyncSession, note_data):
+
+    with pytest.raises(IntegrityError):
+        await create_note(note_data=note_data, user_id=100, db=db)
