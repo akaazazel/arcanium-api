eb0bfe8bd0313f7a0b3180781409fb43ba5c82a1
akaazazel <cvakshay764@gmail.com>
2026-06-20 01:24:41 +0530

    feat: add test for login route

--- tests/test_routes/test_routes_auth.py
+++ tests/test_routes/test_routes_auth.py
@@ -1,14 +1,17 @@
 import asyncio
+from datetime import UTC, datetime, timedelta
+from unittest.mock import AsyncMock, Mock, patch
 
+import jwt
 import pytest
 import pytest_asyncio
+from app.core.settings import settings
 from app.database import get_db
 from app.main import app
-from app.schemas.schemas import UserCreate, UserResponse
-from fastapi import HTTPException
+from app.schemas.schemas import Token, UserCreate, UserResponse
 from fastapi.testclient import TestClient
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from tests.test_services.fixtures import db, init_db
+from app.core.exceptions import UnauthorizedError
 
 client = TestClient(app=app)
 
@@ -19,6 +22,18 @@ def override_db(db):
         yield db
 
     app.dependency_overrides[get_db] = give_db
+
+
+def create_mock_token(type: str):
+    return jwt.encode(
+        payload={
+            "sub": "user",
+            "exp": datetime.now(UTC) + timedelta(days=10),
+            "type": type,
+        },
+        key=settings.secret_key,
+        algorithm=settings.algorithm,
+    )
 
 
 @pytest.mark.asyncio
@@ -57,3 +72,45 @@ async def test_register_duplicate_user(override_db):
     )
 
     assert response.status_code == 409
+
+
+@pytest.mark.asyncio
+@patch("app.routes.auth.login_user", new_callable=AsyncMock)
+async def test_login_user(mock_login_user, override_db):
+
+    access = Token(
+        access_token=create_mock_token("access"),
+        token_type="access",
+    )
+    refresh = create_mock_token("refresh")
+
+    mock_login_user.return_value = (access, refresh)
+
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": "user1@gmail.com",
+            "password": "user1password",
+        },
+    )
+
+    assert response.status_code == 200
+    assert response.cookies.get("refresh_token") == refresh
+    assert response.json()["access_token"] == access.access_token
+
+
+@pytest.mark.asyncio
+@patch("app.routes.auth.login_user", new_callable=AsyncMock)
+async def test_login_exception(mock_login_user):
+
+    mock_login_user.side_effect = UnauthorizedError
+
+    response = client.post(
+        "/auth/login",
+        data={
+            "username": "user1@gmail.com",
+            "password": "user1password",
+        },
+    )
+
+    assert response.status_code == 401
